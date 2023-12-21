6653172053e3ed8f91e6c08ca9d0247940932fe2
ep4518 <edward.peterson18@imperial.ac.uk>
2023-12-21 09:37:53 +0100

    Add day 10 part 2

--- Day10/day10_2.py
+++ Day10/day10_2.py
@@ -1,21 +1,166 @@
 #!/usr/bin/env python3
 import sys
-import numpy as np
 
+class MazeDecoder:
+    def decode(self, rows: list[str]) -> int:
+        grid = self.parse(rows)
+        start = self.find_start(grid)
+        locations = []
+        visited = set()
+        locations.append(start)
+        locations.append((start[0]+1,start[1]))
+        visited.add(start)
+
+        break_outer = False
+        for i, row in enumerate(grid):
+            for j in range(len(row)):
+                next_location = self.take_step(grid, locations[-1], locations[-2])
+                if next_location != start:
+                    locations.append(next_location)
+                    visited.add(next_location)
+                else:
+                    break_outer = True
+                    break
+
+            if break_outer:
+                break
+
+        return locations, visited
+
+
+    def parse(self, rows: list[str]) -> list[list[str]]:
+        new = []
+        for row in rows:
+            row = row.replace('\n','')
+            new_row = []
+            [new_row.append(element) for element in row]
+            new.append(new_row)
+
+        return new
+    
+    def find_start(self, grid: list[list[str]]) -> tuple:
+        for i, row in enumerate(grid):
+            for j in range(len(row)):
+                if grid[i][j] == 'S':
+                    start = (i,j)
+        return start
+    
+    def create_maze_plot(self, grid_size: tuple, locations: list[tuple]) -> None:
+        maze = [[' ' for _ in range(grid_size[1])] for _ in range(grid_size[0])]
+
+        for y, x in locations:
+            maze[y][x] = '*'
+
+        for row in maze:
+            print(''.join(row))
+    
+    def take_step(self, grid: list[list[str]], location: tuple, previous_location: tuple) -> tuple:
+        x, y = location[1], location[0]
+        prev_x, prev_y = previous_location[1], previous_location[0]
+        right_bound, bottom_bound = len(grid[1]), len(grid)
+        eight_directions = {
+            "up": (x,y-1),
+            "down": (x,y+1),
+            "right": (x+1,y),
+            "left": (x-1,y),
+            "left_up": (x-1,y-1),
+            "left_down": (x-1,y+1),
+            "right_up": (x+1,y-1),
+            "right_down": (x+1,y+1),
+        }
+
+        # print(f"{grid[y][x], x, y}")
+        X = grid[y][x] 
+
+        if X == '|':
+            y = 2 * y - prev_y
+            return (y,x)
+        if X == '-':
+            x = 2 * x - prev_x
+            return (y,x)
+        if X == 'F':
+            if x == prev_x:
+                x = x + 1
+                return (y,x)
+            elif y == prev_y:
+                y = y + 1
+                return (y,x)
+        if X == 'J':
+            if x == prev_x:
+                x = x - 1
+                return (y,x)
+            elif y == prev_y:
+                y = y - 1
+                return (y,x)
+        if X == 'L':
+            if x == prev_x:
+                x = x + 1
+                return (y,x)
+            elif y == prev_y:
+                y = y - 1
+                return (y,x)
+        if X == '7':
+            if x == prev_x:
+                x = x - 1
+                return (y,x)
+            elif y == prev_y:
+                y = y + 1
+                return (y,x)
+
+    def count_interior_points(self, rows: list[str]) -> int:
+        part2 = 0
+        for line in rows:
+            line = line.replace('\n', '')
+            print(line)
+            outside = True
+            startF = None
+            for ch in line:
+                if ch == 'S':
+                    ch = 'F'
+                    match ch:
+                        case ".":
+                            if not outside:
+                                part2 += 1
+                        case "|":
+                            outside = not outside
+                        case "F":
+                            startF = True
+                        case "L":
+                            startF = False
+                        case "-":
+                            assert not startF is None
+                        case "7":
+                            assert not startF is None
+                            if not startF:
+                                outside = not outside
+                            startF = None
+                        case "J":
+                            assert not startF is None
+                            if startF:
+                                outside = not outside
+                            startF = None
+                        case _:
+                            print(f"Unexpected character: {ch}")
+
+        return part2
+
+
+
+    
 def main():
     if len(sys.argv) != 2:
-        print("Improper Usage: python day5.py [.txt]")
+        print("Improper Usage: python day9.py [.txt]")
         sys.exit(1)
 
     try:
         with open(sys.argv[1], 'r') as file:
-            score = 0
             rows = file.readlines()
-            rows, count = parse(rows)
-            for row in rows:
-                score += extrapolate_backward(row)
+            maze_decoder = MazeDecoder()
+            locations, _ = maze_decoder.decode(rows)
+            maze_decoder.create_maze_plot((len(rows), len(rows[0].strip())), locations)
 
-            print(score)
+            part2 = maze_decoder.count_interior_points(rows)
+            print(f"Interior points: {part2}")
 
     except FileNotFoundError:
         print(f"File '{sys.argv[1]}' not found.")
@@ -26,34 +171,7 @@ def main():
 
     return 0
 
-def parse(rows):
-    count = 0
-    new = []
-    for i, row in enumerate(rows):
-        count += 1
-        row = row.replace('\n', '')
-        row = row.split()
-        row = [int(element) for element in row]
-        new.append(row)
-
-    return new, count
-
-def extrapolate_backward(row):
-    tmp_padded = np.concatenate(([0], np.array(row)))
-    tmp = tmp_padded
-    result_matrix = tmp.reshape(1, -1)
-
-    while not np.all(tmp_padded == 0):
-        tmp = np.diff(tmp)
-        tmp_padded = np.pad(tmp, (0, result_matrix.shape[1] - len(tmp)), mode='constant')
-        tmp_padded[0] = 0
-        result_matrix = np.vstack([result_matrix, tmp_padded])
-
-    for i in range(result_matrix.shape[1] - len(tmp)):
-        k = result_matrix.shape[1] - len(tmp) - i - 1
-        result_matrix[k, 0] = result_matrix[k, 1] - result_matrix[k + 1, 0]
-
-    return result_matrix[0,0]
-
+ 
 if __name__ == "__main__":
     main()
+
