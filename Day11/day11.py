2673d5529fed81f035c83366ad6c95c0fe5f65ea
ep4518 <edwardpeteson18@imperial.ac.uk>
2023-12-23 20:11:40 +0000

    Add day 11 part 1

---
+++ Day11/day11.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+import sys
+from itertools import combinations
+
+class universeDecoder:
+    def decode(self, rows):
+        total = 0
+        grid = self.parse(rows)
+        expandedGrid = self.expand(grid)
+        galaxies = self.find_galaxies(expandedGrid)
+        galaxyPairs = combinations(galaxies.keys(), 2)
+
+        for pair in list(galaxyPairs):
+            distance = self.calculate_distance(galaxies[pair[0]], galaxies[pair[1]])
+            total += distance
+
+        return total
+    
+    def calculate_distance(self, galaxy1: tuple[int, int], galaxy2: tuple[int, int]) -> int:
+        x1, y1 = galaxy1
+        x2, y2 = galaxy2
+
+        return abs(x1 - x2) + abs(y1 - y2)
+    
+    def find_galaxies(self, grid: list[list[str]]) -> dict:
+        galaxies = {}
+        cnt = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == '#':
+                    cnt += 1
+                    galaxies[f"galaxy{cnt}"] = (i, j)
+        
+        return galaxies
+
+    def expand(self, grid: list[list[str]]) -> list[list[str]]:
+        grid = self.add_rows(grid)
+
+        transpose = [list(row) for row in zip(*grid)]
+
+        transpose = self.add_rows(transpose)
+
+        grid  = [list(row) for row in zip(*transpose)]
+                
+        return grid
+    
+    def add_rows(self, grid: list[list[str]]) -> list[list[str]]:
+        new = []
+        for row in grid:
+            if not any(ch == '#' for ch in row):
+                new.append(row)
+            new.append(row)
+        
+        return new
+
+    def parse(self, rows: list[str]) -> list[list[str]]:
+        new = []
+        for row in rows:
+            row = row.replace('\n', '')
+            new_row = []
+            [new_row.append(element) for element in row]
+            new.append(new_row)
+
+        return new
+
+def main():
+    if len(sys.argv) != 2:
+        print("Improper Usage: python day11.py [.txt]")
+        sys.exit(1)
+
+    try:
+        with open(sys.argv[1], 'r') as file:
+            rows = file.readlines()
+            universe = universeDecoder()
+            Part1 = universe.decode(rows=rows)
+            print(f"Part 1: {Part1}")
+
+
+    except FileNotFoundError:
+        print(f"File '{sys.argv[1]}' not found.")
+    except PermissionError:
+        print(f"Permission denied for '{sys.argv[1]}'.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    return 0
+
+if __name__ == "__main__":
+    main()
