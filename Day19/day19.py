eaab58f713f281d6657e033d203a5424d220a4b3
ep4518 <edward.peterson18@imperial.ac.uk>
2024-01-07 16:37:23 +0400

    Add .json for debugging

---
+++ Day19/day19.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+import sys
+from typing import Dict
+
+class Decoder:
+    def part1(self, rows):
+        total = 0
+        assignments = []
+        workflows, parts = self.parse(rows=rows)
+        for part in parts:
+            print(part)
+            assignments.append(self.assign(part=part, workflows=workflows))
+        
+        for assignment in zip(assignments, parts):
+            if assignment[0]:
+                total += sum(value for value in assignment[1].values())
+        print(assignments)
+        return total
+
+    def part2(self, rows):
+        return 0
+    
+    def assign(self, part, workflows):
+        workflow =  workflows["in"]
+        while workflow[:-1]:
+            for elem in workflow[:-1]:
+                print(elem)
+                colon = elem.find(':')
+                part_val, sign, val = part[elem[0]], elem[1], int(elem[2:colon])
+                print(part_val, sign, val)
+                if (sign == '>' and part_val > val) or (sign == '<' and part_val < val):
+                    print(elem[(colon + 1):])
+                    if elem[(colon + 1):] == 'A':
+                        return 1
+                    elif elem[(colon + 1):] == 'R':
+                        return 0
+                    else: 
+                        print('test') 
+                        workflow = workflows[f"{elem[(colon + 1):]}"]
+                        break
+            print(workflow[-1])
+            if workflow[-1] == 'A':
+                return 1
+            elif workflow[-1] == 'R':
+                return 0
+            else:
+                workflow = workflows[f"{workflow[-1]}"]
+                                
+
+
+    def parse(self, rows):
+        workflows =[]
+        parts = []
+        flag = True
+        for row in rows:
+            row = row.replace('\n','')
+            skip = False
+            if row == '':
+                flag = False
+                skip = True
+            if flag:
+                workflows.append(row)
+            else:
+                if not skip:
+                    parts.append(row)
+
+        workflows2 = {}
+        for workflow in workflows:
+            workflow = workflow.replace('}', '').split('{')
+            rhs = workflow[1].split(',')
+            workflows2[workflow[0]] = rhs
+        
+        parts2 = []
+        for part in parts:
+            part = part.replace('{', '').replace('}', '')
+            part = part.split(',')
+            part_dict = {}
+            for elem in part:
+                part_dict[elem[0]]= int(elem[2:])
+            parts2.append(part_dict)
+
+        return workflows2, parts2
+        
+def main():
+    if len(sys.argv) != 2:
+        print("Improper Usage: python day19.py [.txt]")
+        sys.exit(1)
+
+    try:
+        with open(sys.argv[1], 'r') as file:
+            rows = file.readlines()
+            decoder_instance = Decoder()
+            Part1 = decoder_instance.part1(rows=rows)
+            Part2 = decoder_instance.part2(rows=rows)
+            print(f"Part 1: {Part1}")
+            print(f"Part 2: {Part2}")
+
+    except FileNotFoundError:
+        print(f"File '{sys.argv[1]}' not found.")
+    except PermissionError:
+        print(f"Permission denied for '{sys.argv[1]}'.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    return 0
+
+if __name__ == "__main__":
+    main()
---
+++ Day19/notmyday19.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+import math
+import sys
+
+
+ops = {"<": int.__lt__, ">": int.__gt__}
+
+
+def apply_workflows(part, workflow):
+    if workflow in "AR":
+        return workflow
+
+    for category, op, num, target in workflows[workflow]:
+        if category == "default" or ops[op](part[category], num):
+            return apply_workflows(part, workflow=target)
+
+    assert False
+
+
+with open(sys.argv[1], "r") as f:
+    raw_workflows, raw_parts = f.read().split("\n\n")
+
+# parse workflows
+workflows = {}
+for line in raw_workflows.splitlines():
+    # px{a<2006:qkq,m>2090:A,rfg}
+    name, raw_rules = line.rstrip("}").split("{")
+    rules = []
+    for rule in raw_rules.split(","):
+        if ":" not in rule:
+            rules.append(("default", None, None, rule))
+            continue
+        condition, target = rule.split(":")
+        category = condition[0]
+        op = condition[1]
+        assert op in "<>"
+        num = int(condition[2:])
+        assert num >= 0
+        rules.append((category, op, num, target))
+    workflows[name] = rules
+
+# loop over parts, if accept add to total
+part1 = 0
+assignments111 = []
+for line in raw_parts.splitlines():
+    # {x=787,m=2655,a=1222,s=2876}
+    values = line.strip("{}").split(",")
+    part = {}
+    for value in values:
+        vtype, v = value.split("=")
+        part[vtype] = int(v)
+    if apply_workflows(part, workflow="in") == "A":
+        assignments111.append(1)
+        part1 += sum(part.values())
+    else:
+        assignments111.append(0)
+
+print(assignments111)
+print(f"Part 1: {part1}")
+
+
+def apply_workflows_ranges(ranges, workflow):
+    if workflow == "R":
+        return 0
+    if workflow == "A":
+        return math.prod(stop - start for start, stop in ranges.values())
+
+    total = 0
+    for category, op, num, target in workflows[workflow]:
+        if category == "default":
+            total += apply_workflows_ranges(ranges, workflow=target)
+            continue
+        start, stop = ranges[category]
+        if op == "<":
+            match_range = (start, min(num, stop))
+            miss_range = (max(num, start), stop)
+        else:
+            match_range = (max(num + 1, start), stop)
+            miss_range = (start, min(num + 1, stop))
+        if match_range[0] < match_range[1]:
+            next_ranges = dict(ranges)
+            next_ranges[category] = match_range
+            total += apply_workflows_ranges(next_ranges, workflow=target)
+        if miss_range[0] < miss_range[1]:
+            ranges = dict(ranges)
+            ranges[category] = miss_range
+        else:
+            break
+
+    return total
+
+
+part2 = apply_workflows_ranges({k: (1, 4001) for k in "xmas"}, workflow="in")
+print(f"Part 2: {part2}")
