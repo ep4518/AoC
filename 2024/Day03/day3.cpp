7829bde7efa8302fda15fc159f2ca744d68033fa
ep4518 <edward.peterson18@imperial.ac.uk>
2024-12-03 14:29:41 +0000

    Add day3.cpp

---
+++ 2024/Day03/day3.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <string>
+#include <regex>
+
+using namespace std;
+
+int main() {
+    ifstream f("input.txt");
+    if (!f.is_open()) {
+        cerr << "Error: Unable to open file." << endl;
+        return 1;
+    }
+    vector< string > rows;
+    string line;
+    while (getline(f, line)) {
+        rows.push_back(line);
+    }
+
+    std::regex mul_pattern(R"(mul\((\d+),\s*(-?\d+)\)|do\(\)|don't\(\))");
+
+    bool flag = true;
+
+    int tot1 = 0, tot2 = 0;
+
+    for (auto line: rows) {
+
+        std::sregex_iterator it(line.begin(), line.end(), mul_pattern);
+        std::sregex_iterator end;
+
+        while(it != end) {
+            std::smatch match = *it;
+
+            cout << match[0] << endl;
+
+            if (match[0].compare("do()") == 0) flag = true;
+            else if (match[0].compare("don't()") == 0) flag = false;
+            else {
+                tot1 += stoi(match[1]) * stoi(match[2]);
+                tot2 += flag * stoi(match[1]) * stoi(match[2]);
+            }
+
+            it++;
+        }
+    }
+    cout << "Part 1: " << tot1 << endl;
+    cout << "Part 2: " << tot2 << endl;
+}
