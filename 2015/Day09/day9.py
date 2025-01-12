9d3c3c4f2481637d6eef007b3ac31bc0c4f9ac74
ep4518 <edward.peterson18@imperial.ac.uk>
2025-01-12 19:26:54 +0000

    Add day9.py

---
+++ 2015/Day09/day9.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import sys
+import re
+from itertools import permutations
+import networkx as nx
+
+pattern = re.compile(r'(?P<loc1>[a-zA-Z]+) to (?P<loc2>[a-zA-Z]+) = (?P<dist>\d+)') 
+data = list(map(lambda x: list(x.groupdict().values()) , list(map(pattern.match, open(sys.argv[1]).read().splitlines()))))
+
+locations = set()
+relations = {}
+
+G = nx.Graph()
+
+for a, b, dist in data:
+    G.add_edge(a, b, weight=int(dist))
+    
+solve = lambda func: (
+    func(sum(relations[trip]['weight'] for trip in zip(route, route[1:]))
+         for route in permutations(locations)))
+
+locations = G.nodes()
+relations = G.edges()
+
+print(solve(min))
+print(solve(max))
